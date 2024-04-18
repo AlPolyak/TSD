b1940ce248c6d7bce4edc2900a4da7ac45be4244
AlPolyak <161193730+AlPolyak@users.noreply.github.com>
2024-04-18 20:52:36 +0300

    Update TSD_GLOBAL.py

--- MY_TSD/TSD_GLOBAL.py
+++ MY_TSD/TSD_GLOBAL.py
@@ -320,7 +320,8 @@ def inputqtty(hashMap,_files=None,_data=None):
         hashMap.put("ShowScreen","Ввод количества")
     else:
         #  просто добавим количество
-        plus1(hashMap,prod,prod["Количество"],_ТСД_Настройки) # Количество из ШК
+        screenmessage(hashMap,json.dumps(prod)) 
+        #plus1(hashMap,prod,prod["Количество"],_ТСД_Настройки) # Количество из ШК
     return hashMap
 
 def plus1(hashMap,prod,qnt,Настройки):
