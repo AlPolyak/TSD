2374ee6d24a0741f50b4794b6200682850a92fff
AlPolyak <161193730+AlPolyak@users.noreply.github.com>
2024-04-18 15:41:44 +0300

    Update TSD_GLOBAL.py

--- MY_TSD/TSD_GLOBAL.py
+++ MY_TSD/TSD_GLOBAL.py
@@ -310,7 +310,7 @@ def cardslist(hashMap,object1):
 def iputqtty(hashMap,_files=None,_data=None):
     cards_prod=json.loads(hashMap.get("cards_prod"))
     selected_card_key=hashMap.get("selected_card_key")
-    screenmessage(hashMap,selected_card_key,"Индекс выбранной номенклатуры")
+    screenmessage(hashMap,str(selected_card_key)+" "+str(selected_card_position),"Индекс выбранной номенклатуры")
     prod=cards_prod["customcards"]["cardsdata"][selected_card_key]
     hashMap.put("_curprod",json.dumps(prod))
     _ТСД_Настройки=json.loads(hashMap.get("_ТСД_Настройки"))
@@ -322,6 +322,7 @@ def iputqtty(hashMap,_files=None,_data=None):
     return hashMap
 
 def plus1(hashMap,prod,qnt):
+    screenmessage(hashMap,selected_card_key,"Индекс выбранной номенклатуры")
     # получим по prod ключи номенклатуры
     # поищем в документе результате эту номенклатуру
     # если нашли, добавим или заменим в зависимости от настройки qnt
