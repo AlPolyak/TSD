8655a4f8362b8054dc81cab4640b59cd5de77ef4
AlPolyak <161193730+AlPolyak@users.noreply.github.com>
2024-04-18 16:58:25 +0300

    Update TSD_GLOBAL.py

--- MY_TSD/TSD_GLOBAL.py
+++ MY_TSD/TSD_GLOBAL.py
@@ -303,7 +303,6 @@ def cardslist(hashMap,object1):
         }
         i=i+1  
         j["customcards"]["cardsdata"].append(c)
-    hashMap=screenmessage(hashMap,j) 
     hashMap.put("cards_prod",json.dumps(j,ensure_ascii=False).encode('utf8').decode())
     return hashMap
 
